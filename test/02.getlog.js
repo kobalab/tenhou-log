178159be9435f5f6a4b7a891a2729dbe780738a4
Satoshi Kobayashi <koba@yk.rim.or.jp>
2019-10-31 17:15:13 +0900

    getlog.js を初期登録

---
+++ test/02.getlog.js
@@ -0,0 +1,19 @@
+const assert  = require('assert');
+
+const getlog = require('../lib/getlog');
+
+suite('getlog()', ()=>{
+    test('モジュールが存在すること', ()=>assert.ok(getlog));
+    test('牌譜が取得できること', (done)=>{
+        getlog('2016031822gm-0009-10011-896da481').then(xml=>{
+            assert.ok(xml);
+            done();
+        });
+    });
+    test('ログID不正', (done)=>{
+        getlog('badid').catch(code=>{
+            assert.equal(code, 404);
+            done();
+        });
+    });
+});
