833951001d51675db4b4a517557709163ad83017
Satoshi Kobayashi <koba@yk.rim.or.jp>
2019-10-31 17:14:45 +0900

    request.js を初期登録

---
+++ test/01.request.js
@@ -0,0 +1,43 @@
+const assert  = require('assert');
+
+const request = require('../lib/request');
+
+suite('request()', ()=>{
+    test('モジュールが存在すること', ()=>assert.ok(request));
+    test('不正なProtocol → 400', (done)=>{
+        request('file:///', '', null, code=>{
+            assert.equal(code, 400);
+            done();
+        });
+    });
+    test('不正なサーバ → 500', (done)=>{
+        request('http://badhost/', '', null, code=>{
+            assert.equal(code, 500);
+            done();
+        });
+    });
+    test('GET', (done)=>{
+        request('http://twitter.com/', '', (body)=>{
+            assert.ok(body.length);
+            done();
+        });
+    });
+    test('GET (gzip)', (done)=>{
+        request('http://twitter.com/', 'gzip', (body)=>{
+            assert.ok(body.length);
+            done();
+        });
+    });
+    test('GET (deflate)', (done)=>{
+        request('http://twitter.com/', 'deflate', (body)=>{
+            assert.ok(body.length);
+            done();
+        });
+    });
+    test('GET (not found)', (done)=>{
+        request('http://kobalab.net/x', '', null, (code)=>{
+            assert.equal(code, 404);
+            done();
+        });
+    });
+});
