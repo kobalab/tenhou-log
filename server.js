928e537d4da2eb71bdac952b1b443232c7719d50
Satoshi Kobayashi <koba@yk.rim.or.jp>
2020-07-05 22:33:21 +0800

    server.js を初期登録

---
+++ server.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const express = require('express');
+const getlog  = require('./lib/getlog');
+const convlog = require('./lib/convlog');
+
+const port = process.env.PORT || 8001;
+const docs = process.env.DOCROOT;
+
+const app = express();
+
+app.get('/tenhou-log/:id.json', (req, res, next)=>{
+    let id = req.params.id;
+    getlog(id)
+        .then(xml=>res.json(convlog(xml, id)))
+        .catch(e=>next());
+});
+if (docs) app.use(express.static(docs));
+app.use((req, res)=>res.status(404).send('<h1>Not Found</h1>'));
+
+app.listen(port, ()=>console.log(`Server start on http://127.0.0.1:${port}`));
