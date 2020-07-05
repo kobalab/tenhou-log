b045335ffeb63a00c8d81b8ee7108d1186e7cd0c
Satoshi Kobayashi <koba@yk.rim.or.jp>
2020-07-05 10:14:33 +0800

    command.js を初期登録

---
+++ command.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const getlog  = require('./lib/getlog');
+const convlog = require('./lib/convlog');
+
+let id = process.argv[2];
+if (! id) {
+    console.error('Usage: tenhou-log logid');
+    return;
+}
+
+getlog(id)
+    .then(xml=>process.stdout.write(JSON.stringify(convlog(xml, id))))
+    .catch(()=>console.error(`${id}: not found.`));
