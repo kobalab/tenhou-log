14028d10968745f3e92ada5b39030e2b02ff8a17
Satoshi Kobayashi <koba@yk.rim.or.jp>
2020-07-19 09:23:50 +0800

    環境変数 → コマンドオプション に変更

--- server.js
+++ server.js
@@ -2,16 +2,26 @@
 
 "use strict";
 
+const yargs   = require('yargs');
 const express = require('express');
 const getlog  = require('./lib/getlog');
 const convlog = require('./lib/convlog');
 
-const port = process.env.PORT || 8001;
-const docs = process.env.DOCROOT;
+const argv = yargs
+    .option('port',    { alias: 'p', default: 8001           })
+    .option('baseurl', { alias: 'b', default: '/tenhou-log/' })
+    .option('docroot', { alias: 'd'                          })
+    .argv;
+
+const port = argv.port;
+const base = (''+argv.baseurl)
+                .replace(/^(?!\/.*)/, '/$&')
+                .replace(/\/$/, '') + '/';
+const docs = argv.docroot;
 
 const app = express();
 
-app.get('/tenhou-log/:id.json', (req, res, next)=>{
+app.get(`${base}:id.json`, (req, res, next)=>{
     let id = req.params.id;
     getlog(id)
         .then(xml=>res.json(convlog(xml, id)))
@@ -20,4 +30,7 @@ app.get('/tenhou-log/:id.json', (req, res, next)=>{
 if (docs) app.use(express.static(docs));
 app.use((req, res)=>res.status(404).send('<h1>Not Found</h1>'));
 
-app.listen(port, ()=>console.log(`Server start on http://127.0.0.1:${port}`));
+app.listen(port,
+    ()=>console.log(
+        `Server start on http://127.0.0.1:${port}${base}`,
+        docs ? `(docroot=${docs})` : ''));
